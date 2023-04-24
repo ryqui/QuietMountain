deee2a912b4e4bfa7cca4dd8f06f303178b75ce3
SeaofChaos <46305438+ryqui@users.noreply.github.com>
2023-04-23 21:21:19 -0400

    Added in basic movement + camera
    Currently added in base movement, a camera, and a dummy avatar with an idle and walk animation set up.
    
    I added in a simple state machine, which, while not completely useful right now, could be beneficial when we add in more actions such as holding the lighter. It will also come in handy when implementing enemies.
    
    Current models provided by marianpekar:
    https://github.com/marianpekar/unity-third-person-controller

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions
+{
+    public Vector2 MouseDelta;
+    public Vector2 MoveComposite;
+
+    public Action OnJumpPerformed;
+
+    private Controls controls;
+
+    private void OnEnable(){
+        if (controls != null) return;
+        
+        controls = new Controls();
+        controls.Player.SetCallbacks(this);
+        controls.Player.Enable();
+    }
+
+    public void OnDisable(){
+        controls.Player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context){
+        MoveComposite = context.ReadValue<Vector2>();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context){
+        return;
+    }
+
+    public void OnSprint(InputAction.CallbackContext context){
+        if (!context.performed) return;
+        return;
+    }
+
+    public void OnLook(InputAction.CallbackContext context){
+        MouseDelta = context.ReadValue<Vector2>();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
