ff42e3880abfcbb604a05e9b825bb87df0eb38b9
jumpstartcoding <116022896+jumpstartcoding@users.noreply.github.com>
2023-05-08 18:33:04 -0400

    Text
    Neighborhood Dialogue

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+[RequireComponent(typeof(InputReader))]
 
 public class Dialogue : MonoBehaviour
 {
@@ -10,19 +11,27 @@ public class Dialogue : MonoBehaviour
     public float textSpeed;
     private int index;
     private bool collide = false;
+    public InputReader InputReader { get; private set; }
+    public bool stopMovement = true; 
+    public Collider col { get; private set; }
     
-
+    void Start()
+    {   col = GetComponent<Collider>(); 
+        InputReader = GetComponent<InputReader>();
+    }
   void OnTriggerEnter(Collider other)
-    {      textComponent.text= string.Empty;
+    {   
+        textComponent.text= string.Empty;
         if (other.CompareTag("Player")) 
         {  collide = true;
+            col.isTrigger = stopMovement ? false : true ;
             StartDialogue();}
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && collide)
+        if(InputReader.interact == true && collide)
         {
             if(textComponent.text == lines[index])
             {
@@ -33,12 +42,13 @@ public class Dialogue : MonoBehaviour
                 StopAllCoroutines();
                 textComponent.text=lines[index];
             }
+            InputReader.interact = false;
         }
     }
 
     void StartDialogue()
     {   index =0;
- 
+
         StartCoroutine(TypeLine());
     }
 
@@ -62,6 +72,7 @@ void NextLine()
     else
     {   textComponent.text= string.Empty;
         gameObject.SetActive(false);
+        
     }
 
 }
